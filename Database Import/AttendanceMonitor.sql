81298b9db3b74fcbb69682715d90a9f020375442
tcyubahi <tcyubahi@asu.edu>
2018-08-19 21:25:41 -0700

    Initializing repo

---
+++ Database Import/AttendanceMonitor.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 20, 2018 at 04:40 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `AttendanceMonitor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activeSession`
+--
+
+CREATE TABLE `activeSession` (
+  `id` int(11) NOT NULL,
+  `session` varchar(60) NOT NULL,
+  `owner` varchar(60) NOT NULL,
+  `time` time NOT NULL,
+  `date` date NOT NULL,
+  `activated` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addAttendee`
+--
+
+CREATE TABLE `addAttendee` (
+  `idNumber` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(20) NOT NULL,
+  `studid` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `session` varchar(20) NOT NULL,
+  `owner` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int(20) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `owner` varchar(60) NOT NULL,
+  `count` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessionsLogs`
+--
+
+CREATE TABLE `sessionsLogs` (
+  `id` int(20) NOT NULL,
+  `date` date NOT NULL,
+  `starttime` time NOT NULL,
+  `endtime` time NOT NULL,
+  `owner` varchar(30) NOT NULL,
+  `session` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(10) NOT NULL,
+  `studid` varchar(20) NOT NULL,
+  `session` varchar(60) NOT NULL,
+  `owner` varchar(60) NOT NULL,
+  `lastname` varchar(60) NOT NULL,
+  `firstname` varchar(600) NOT NULL,
+  `asurite` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `userkey` varchar(255) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activeSession`
+--
+ALTER TABLE `activeSession`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `logs_ibfk_1` (`owner`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`title`,`owner`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `owner` (`owner`);
+
+--
+-- Indexes for table `sessionsLogs`
+--
+ALTER TABLE `sessionsLogs`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `sessionsLogs_ibfk_1` (`owner`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`studid`,`session`,`owner`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `asurite` (`asurite`),
+  ADD KEY `session` (`session`),
+  ADD KEY `owner` (`owner`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activeSession`
+--
+ALTER TABLE `activeSession`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=222;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sessionsLogs`
+--
+ALTER TABLE `sessionsLogs`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`);
+
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`);
+
+--
+-- Constraints for table `sessionsLogs`
+--
+ALTER TABLE `sessionsLogs`
+  ADD CONSTRAINT `sessionsLogs_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`session`) REFERENCES `sessions` (`title`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
